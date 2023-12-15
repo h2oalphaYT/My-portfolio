1c8c387986fd7c876a41eca3f4a4b04238272329
h2oalphaYT <it21802676@my.sliit.lk>
2023-12-15 17:55:45 +0530

    projects part

--- portfolio/src/componets/Projects/Projects.jsx
+++ portfolio/src/componets/Projects/Projects.jsx
@@ -1,17 +1,35 @@
-
-
+import React, { useState, useEffect } from 'react';
 import styles from "./Projects.module.css";
 import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
+  const [visibleProjects, setVisibleProjects] = useState(projects.slice(0, 3));
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const rotateProjects = () => {
+    const nextIndex = (currentIndex + 1) % projects.length;
+    setCurrentIndex(nextIndex);
+    const newProjects = [...visibleProjects.slice(1), projects[nextIndex]];
+    setVisibleProjects(newProjects);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(rotateProjects, 3000);
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
   return (
     <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
       <div className={styles.projects}>
-        {projects.map((project, id) => {
-          return <ProjectCard key={id} project={project} />;
-        })}
+        {visibleProjects.map((project, index) => (
+          <ProjectCard
+            key={project.id || index}  // Use unique project id if available
+            project={project}
+            animationClass={styles.projectEnter} // Assuming you have defined this class in your CSS
+          />
+        ))}
       </div>
     </section>
   );
