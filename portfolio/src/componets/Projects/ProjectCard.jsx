86e7cf3cc58bf1e47e60297ccde5b7332a793c76
h2oalphaYT <it21802676@my.sliit.lk>
2023-12-15 17:55:33 +0530

    Update ProjectCard.jsx

--- portfolio/src/componets/Projects/ProjectCard.jsx
+++ portfolio/src/componets/Projects/ProjectCard.jsx
@@ -1,12 +1,13 @@
-
+import React from 'react';
 import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
 export const ProjectCard = ({
-  project:{ title, imageSrc, description, skills, demo, source },
+  project: { title, imageSrc, description, skills, demo, source },
+  animationClass
 }) => {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${animationClass}`}>
       <img
         src={getImageUrl(imageSrc)}
         alt={`Image of ${title}`}
@@ -15,19 +16,17 @@ export const ProjectCard = ({
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
       <ul className={styles.skills}>
-        {skills.map((skill, id) => {
-          return (
-            <li key={id} className={styles.skill}>
-              {skill}
-            </li>
-          );
-        })}
+        {skills.map((skill, index) => (
+          <li key={index} className={styles.skill}>
+            {skill}
+          </li>
+        ))}
       </ul>
       <div className={styles.links}>
-        <a href={demo} className={styles.link}>
+        <a href={demo} className={styles.link} target="_blank" rel="noopener noreferrer">
           Demo
         </a>
-        <a href={source} className={styles.link}>
+        <a href={source} className={styles.link} target="_blank" rel="noopener noreferrer">
           Source
         </a>
       </div>
