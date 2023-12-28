2b31b304dc263fa91650ff3096f2db6167000240
h2oalphaYT <it21802676@my.sliit.lk>
2023-12-28 14:50:19 +0530

    yrt

--- portfolio/src/componets/Hero/Hero.jsx
+++ portfolio/src/componets/Hero/Hero.jsx
@@ -27,7 +27,7 @@ export const Hero = () => {
     };
   }, []);
 
-  const cvUrl =  'http://localhost:5173/public/My_CV.pdf.pdf';
+  const cvUrl =  'http://localhost:5173/public/My_CV.pdf';
   const backgroundImageUrl = getImageUrl("hero/stars.png");
 
 
